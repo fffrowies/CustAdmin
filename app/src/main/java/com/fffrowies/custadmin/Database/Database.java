f38b7f02c463052058001a9f797b3f709af7c9f7
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-13 19:37:35 -0300

    Emplaced db in assets folder, created Model and Database with methods

---
+++ app/src/main/java/com/fffrowies/custadmin/Database/Database.java
@@ -0,0 +1,106 @@
+package com.fffrowies.custadmin.Database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.fffrowies.custadmin.Model.Customer;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database extends SQLiteAssetHelper {
+
+    private static final String DB_NAME = "customer.db";
+    private static final int DB_VER = 1;
+
+    public Database(Context context) {
+        super(context, DB_NAME, null, DB_VER);
+    }
+
+    //Function get all customers
+    public List<Customer> getCustomers() {
+
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        // Make sure all is column name in your Table
+        String[] sqlSelect = { "Id", "Name", "Address", "Email", "Phone" };
+        String tableName = "Customers"; // Make sure this is your table name
+
+        qb.setTables(tableName);
+        Cursor cursor = qb.query(db, sqlSelect, null, null,
+                null, null, null);
+        List<Customer> result = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                Customer customer = new Customer();
+                customer.setId(cursor.getInt(cursor.getColumnIndex("Id")));
+                customer.setName(cursor.getString(cursor.getColumnIndex("Name")));
+                customer.setAddress(cursor.getString(cursor.getColumnIndex("Address")));
+                customer.setEmail(cursor.getString(cursor.getColumnIndex("Email")));
+                customer.setPhone(cursor.getString(cursor.getColumnIndex("Phone")));
+
+                result.add(customer);
+            } while (cursor.moveToNext());
+        }
+        return result;
+    }
+
+    //Function get all customer's name
+    public List<String> getNames() {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        // Make sure all is column name in your Table
+        String[] sqlSelect = { "Name" };
+        String tableName = "Customers"; // Make sure this is your table name
+
+        qb.setTables(tableName);
+        Cursor cursor = qb.query(db, sqlSelect, null, null,
+                null, null, null);
+        List<String> result = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                result.add(cursor.getString(cursor.getColumnIndex("Name")));
+            } while (cursor.moveToNext());
+        }
+        return result;
+    }
+
+    //Function get customer by name
+    public List<Customer> getCustomerByName(String name) {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        //Make sure all is column name in your Table
+        String[] sqlSelect = { "Id", "Name", "Address", "Email", "Phone" };
+        String tableName = "Customers"; // Make sure this is your table name
+
+        qb.setTables(tableName);
+
+        //If you want to get extract name, just change
+//        Cursor cursor = qb.query(db, sqlSelect, "Name = ?", new String[]{name},
+//                null, null, null);
+
+        //This will like query : SELECT * FROM Customers WHERE Name LIKE %pattern%
+        Cursor cursor = qb.query(db, sqlSelect, "Name LIKE ?", new String[]{"%"+name+"%"},
+                null, null, null);
+        List<Customer> result = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                Customer customer = new Customer();
+                customer.setId(cursor.getInt(cursor.getColumnIndex("Id")));
+                customer.setName(cursor.getString(cursor.getColumnIndex("Name")));
+                customer.setAddress(cursor.getString(cursor.getColumnIndex("Address")));
+                customer.setEmail(cursor.getString(cursor.getColumnIndex("Email")));
+                customer.setPhone(cursor.getString(cursor.getColumnIndex("Phone")));
+
+                result.add(customer);
+            } while (cursor.moveToNext());
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/fffrowies/custadmin/Model/Customer.java
@@ -0,0 +1,57 @@
+package com.fffrowies.custadmin.Model;
+
+public class Customer {
+    public int id;
+    public String name, address, email, phone;
+
+    public Customer(int id, String name, String address, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Customer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
