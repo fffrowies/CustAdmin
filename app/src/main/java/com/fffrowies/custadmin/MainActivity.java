c7653a8829bad13274585374571c6b62be317563
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-13 20:43:26 -0300

    Added material search bar

--- app/src/main/java/com/fffrowies/custadmin/MainActivity.java
+++ app/src/main/java/com/fffrowies/custadmin/MainActivity.java
@@ -4,9 +4,31 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fffrowies.custadmin.Adapter.SearchAdapter;
+import com.fffrowies.custadmin.Database.Database;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    SearchAdapter adapter;
+
+    MaterialSearchBar materialSearchBar;
+    List<String> suggestList = new ArrayList<>();
+
+    Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +43,92 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //init View
+        recyclerView = findViewById(R.id.recycler_search);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        materialSearchBar = findViewById(R.id.search_bar);
+
+        //Init DB
+        database = new Database(this);
+
+        //Setup search bar
+        materialSearchBar.setHint(getString(R.string.search));
+        materialSearchBar.setCardViewElevation(10);
+        loadSuggestList();
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                List<String> suggest = new ArrayList<>();
+                for(String search:suggestList) {
+                    if (search.toLowerCase().contains(materialSearchBar.getText().toLowerCase())) {
+                        suggest.add(search);
+                    }
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if (!enabled) {
+                    //If close Search, just restore default
+                    adapter = new SearchAdapter(getBaseContext(), database.getCustomers());
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text.toString());
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+        //Init Adapter default set all result
+        adapter = new SearchAdapter(this, database.getCustomers());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void startSearch(String text) {
+
+        adapter = new SearchAdapter(this, database.getCustomerByName(text));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void loadSuggestList() {
+        suggestList = database.getNames();
+        materialSearchBar.setLastSuggestions(suggestList);
+    }
+
+    public void onCardClick(View view) {
+
+        Toast.makeText(this, "Que lo pario", Toast.LENGTH_SHORT).show();
+
+        TextView nameTextView = view.findViewById(R.id.name);
+
+        String name = nameTextView.getText().toString();
+        Toast.makeText(MainActivity.this, "Vamos " + name, Toast.LENGTH_SHORT).show();
     }
 }
+
