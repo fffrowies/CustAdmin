1286e32704baab0f3a1881f85a8fc94af4eba661
fffrowies <fernandorowies.saxo@gmail.com>
2019-01-18 07:34:42 -0300

    sorted modifications

--- app/src/main/java/com/fffrowies/custadmin/Adapter/SearchAdapter.java
+++ app/src/main/java/com/fffrowies/custadmin/Adapter/SearchAdapter.java
@@ -1,7 +1,9 @@
 package com.fffrowies.custadmin.Adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -16,10 +18,14 @@ import com.fffrowies.custadmin.R;
 
 class SearchViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
 
+    //TODO check if it can be private
     public TextView name, address, email, phone;
+    public CardView root_view;
 
     public SearchViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        root_view = itemView.findViewById(R.id.root_view);
         name = itemView.findViewById(R.id.name);
         address = itemView.findViewById(R.id.address);
         email = itemView.findViewById(R.id.email);
@@ -67,6 +73,10 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchViewHolder> {
         holder.address.setText(customers.get(position).getAddress());
         holder.email.setText(customers.get(position).getEmail());
         holder.phone.setText(customers.get(position).getPhone());
+
+        if (position % 2 == 0) {
+            holder.root_view.setCardBackgroundColor(Color.parseColor("#E1E1E1"));
+        }
     }
 
     @Override
