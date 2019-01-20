1925f23839c6de73ab270f0f695dac1f7626ad3c
fffrowies <fernandorowies.saxo@gmail.com>
2019-01-20 18:16:42 -0300

    minor corrections

--- app/src/main/java/com/fffrowies/custadmin/Invoicing.java
+++ app/src/main/java/com/fffrowies/custadmin/Invoicing.java
@@ -28,7 +28,8 @@ public class Invoicing extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_invoicing);
+
         setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
